31505a6acbb74cf542bf0a87134b268ae0b6758a
wcrozier12 <wcc8@nau.edu>
2017-11-29 18:27:05 -0700

    changed package.json

--- server.js
+++ server.js
@@ -29,6 +29,25 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Pass to next layer of middleware
+  next();
+});
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
